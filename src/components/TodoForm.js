53ab5c12e998d2e6e62db3fc5e15e616fedbedc1
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-31 17:20:42 -0700

    playing with reactstrap styles

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Form, FormGroup, Input, Card, CardBody } from "reactstrap";
 // - `<TodoForm>` will hold your input field and your `Add Todo` and `Clear Completed` buttons.
 // - Your input field should take in user input, and allow a user to press `Enter` or click on the `Submit Button` to add a todo to your list.
 // - Once a todo is submitted, the Todo List should re-render and show the added todo.
@@ -31,20 +32,29 @@ class TodoForm extends React.Component {
   render() {
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
-          <input
+      <Card>
+        <CardBody inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+          <Input
             type="text"
             name="todo"
             value={this.state.todo}
             placeholder="Next step in saving the world here"
             onChange={this.handleChange}
           />
-          <button type="submit">Add Todo</button>
-        </form>
-        <button
-          onClick={this.props.removeCompleted}
-        >Clear Completed</button>
+          <Button type="submit">Add Todo</Button>
+          <Button color="danger"
+            onClick={this.props.removeCompleted}
+          >Clear Completed</Button>
+          </FormGroup>
+        </Form>
+        </CardBody>
+
+      </Card>
+      <Card>How TF is this a Card????</Card>
       </>
+
     );
   }
 };
